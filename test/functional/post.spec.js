757359c4d4061fa701d43baef6628a08bc77dd36
Rodolpho Netto <rodolpho.netto@gmail.com>
2020-02-18 21:42:30 -0300

    ajustado questões que envolvem teste

--- test/functional/post.spec.js
+++ test/functional/post.spec.js
@@ -9,11 +9,10 @@ test("Can create posts", async ({ client }) => {
   const response = await client
     .post("/posts/new")
     .send({
-      author: 5,
       content: "Teste de postagem"
     })
     .end();
   response.assertStatus(200);
   const post = await Post.firstOrFail();
-  response.assertJSON({ post: post.toJSON });
+  response.assertJSON({ post: post.toJSON() });
 });
